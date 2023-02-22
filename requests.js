a02abfb871a88e66786b98aaabc9b4de5c3c6ed4
Rodrigo Cezar Leão <rodrigo.leao@radixeng.com.br>
2023-02-21 21:02:01 -0300

    finalizando CRUD com token de utilização

--- requests.js
+++ requests.js
@@ -1,8 +1,20 @@
 
 const API_URL = "https://api-sa-east-1.hygraph.com/v2/cledd0zbo4llj01tbh7gh6ygz/master";
+const GRAPH_CMS_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2NzcwMjE1NjksImF1ZCI6WyJodHRwczovL2FwaS1zYS1lYXN0LTEuaHlncmFwaC5jb20vdjIvY2xlZGQwemJvNGxsajAxdGJoN2doNnlnei9tYXN0ZXIiLCJtYW5hZ2VtZW50LW5leHQuZ3JhcGhjbXMuY29tIl0sImlzcyI6Imh0dHBzOi8vbWFuYWdlbWVudC5ncmFwaGNtcy5jb20vIiwic3ViIjoiZDE5NjNlNzEtNDRjNy00ODMxLWE4MDItMWYxOTQ0MjkyMjgxIiwianRpIjoiY2xlZXZjY3l5Njk2NzAxdWZmamlvOHE1MSJ9.yqgh1pjH4Cuk9e8ttfINTLWI9x__k7diSYJasE1ehM33gioBupTKF1IS9Lli04eWcPpTy4Qx4vcDOBggsOVNGIWWgIoK1i39DWjdtI-aHUgPVOstUTsNJ6nb1FRdhNZYEO_TwjV7Oa83S1OqeMHX-V0v38-2b9pmipWlGm2IAi5tEg-aRCowfJ65ruSHs_zIB7IPf71Oh_we6S4rtEDT_qYAEt2_3VqntvsMtsX2XgdWa1tpB72XENb8RdBmPbM35FY99-kf30aE01Q4ffI10rVNREq-IvViwHr0zZdOh8-ZJrCjsYPjs33zWU0eeQCTAtU5FbZ53_wQtlFfV54rta3lAQhE7tFWIKclbewB-RaDGQzLBXllfzTcvTyNOMf5ZTYpfb6FeCuZidczIFnvkHhVf2-ou-tB5oWau1P47N490s9DWZNDDPJg-VPnrRcwLumpnhuHjxMnnxvZGUxUH3lj_K1Y_vMogkWwB5KzPVXU76T5AMtSAKO0kFQQxzHg1WxHZcGJxpyLsZqCIrwv1xW5db3AYUVpuTYjBS-p8OEfNNzMXXsjlIuKPWrE6c0ACC36oaIQX89LmapoOtaLlpOM6OMKRkKToSOLzlmEmS2DZbJUYXV_A-HqZwWFfJwWj6t5orLWRwb3SOO96uX85FHtLQncZMimyEg3OIbYvw4";
+
+const baseGraphCMSFetch = (mutation) => {
+    return fetch(API_URL, {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json",
+            "authorization": `Bearer ${GRAPH_CMS_TOKEN}`
+        },
+        body: JSON.stringify(mutation)
+    }).then(resp => resp.json())    
+}
 
 const getSmallGroups = () => {
-    const request = { 
+    const cmsQuery = { 
         query : `
             query SmallGroups {
                 smallGroups {
@@ -14,43 +26,14 @@ const getSmallGroups = () => {
             }
     `};
 
-    fetch(API_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(request)
-    }).then(resp => resp.json())
-    .then(data => console.log(data?.data?.smallGroups));
+    baseGraphCMSFetch(cmsQuery)
+        .then(data => console.log(data?.data?.smallGroups));    
 }
 
-const insertSmallGroup = () => {
-    const request = {
-        query: `
-            mutation {
-                createSmallGroup(data: {
-                    name: "Teste",
-                    description: "Descrição teste"
-                    nameSlug: "teste"
-                }) {
-                    id
-                }
-            }
-        `
-    };
+getSmallGroups();
 
-    fetch(API_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(request)
-    }).then(resp => resp.json())
-    .then(data => {
-        console.log(data);
-        // publishSmallGroup(data?.data?.createSmallGroup?.id);
-    });
-}
-
-const publishSmallGroup = (id) => {
-    console.log("🚀 ~ file: requests.js:51 ~ publishSmallGroup ~ id:", id)
-    const request = {
+const publishSmallGroup = (id) => {    
+    const cmsQuery = {
         query: `
             mutation {
                 publishSmallGroup(to:PUBLISHED, where:{id: "${id}"}) {
@@ -60,14 +43,61 @@ const publishSmallGroup = (id) => {
         `
     };
 
-    fetch(API_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(request)
-    }).then(resp => resp.json())
-    .then(data => console.log(data));
+    baseGraphCMSFetch(cmsQuery);
 }
 
-// getSmallGroups();
-insertSmallGroup();
-// publishSmallGroup("cledezlcj8s420blxs0kgcxvk");
+const insertSmallGroup = (name, description) => {
+    const slug = generateSlug(name);
+
+    const cmsQuery = {
+        query: `
+            mutation {
+                createSmallGroup(data: {
+                    name: "${name}",
+                    description: "${description}",
+                    nameSlug: "${slug}"
+                }) {
+                    id
+                }
+            }
+        `
+    };
+
+    baseGraphCMSFetch(cmsQuery)
+        .then(data => publishSmallGroup(data?.data?.createSmallGroup?.id));
+}
+
+const deleteSmallGroup = (id) => {
+    const cmsQuery = {
+        query: `
+            mutation {
+                deleteSmallGroup(where:{id: "${id}"}) {
+                    id
+                }
+            }
+        `
+    };
+
+    baseGraphCMSFetch(cmsQuery)
+}
+
+const updateSmallGroup = (name, description, id) => {
+    const slug = generateSlug(name);
+
+    const cmsQuery = {
+        query: `
+            mutation {
+                updateSmallGroup(data: {
+                    name: "${name}",
+                    description: "${description}",
+                    nameSlug: "${slug}"
+                },where: {id: "${id}"}) {
+                    id
+                }
+            }
+        `
+    };
+
+    baseGraphCMSFetch(cmsQuery)
+        .then(data => publishSmallGroup(id));
+}
